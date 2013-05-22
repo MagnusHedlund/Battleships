c24bd6f0444de001bcd93306b8dbc9ae79e3cafa
Magnus Hedlund <magnus.hedlund.mh@gmail.com>
2013-05-22 12:04:06 +0200

    Added message files

---
+++ Client/src/messages/HitMessage.java
@@ -0,0 +1,27 @@
+package battleships.message;
+
+import battleships.Coordinate;
+import battleships.Ship;
+
+public class HitMessage {
+	private boolean isHit=false;
+	private Coordinate coordinate=new Coordinate(1,1);
+	private boolean isSunk=false;
+	private Ship ship=null;
+	
+	public HitMessage(){
+		
+	}
+	/* Getters */
+	public boolean getIsHit(){return isHit;}
+	public Coordinate getCoordinate(){return coordinate;}
+	public boolean getIsSunk(){return isSunk;}
+	public Ship getShip(){return ship;}
+	
+	/* Setters */
+	public void setIsHit(boolean isHit){this.isHit=isHit;}
+	public void setCoordinate(Coordinate shotCoordinate){coordinate=shotCoordinate;}
+	public void setIsSunk(boolean isSunk){this.isSunk=isSunk;}
+	public void setShip(Ship sunkShip){ship=sunkShip;}
+}
+

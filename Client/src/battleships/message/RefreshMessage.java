555cdffaf3967f98ab86cc19d3268ef82874c52b
Magnus Hedlund <magnus.hedlund.mh@gmail.com>
2013-05-29 23:01:58 +0200

    added Refresh messages

---
+++ Client/src/battleships/message/RefreshMessage.java
@@ -0,0 +1,11 @@
+package battleships.message;
+
+public class RefreshMessage extends Message {
+
+	private static final String myType="RefreshMessage";
+	public RefreshMessage() {
+		super(myType);
+		// TODO Auto-generated constructor stub
+	}
+
+}

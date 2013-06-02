e2bc2145a2ddd4c5498ae5772a3aace6748fbdaa
Magnus Hedlund <magnus.hedlund.mh@gmail.com>
2013-06-02 20:50:42 +0200

    added comments and deleted some messages in Session.java

--- Server/src/battleships/server/Session.java
+++ Server/src/battleships/server/Session.java
@@ -16,22 +16,72 @@ import battleships.message.ValidationMessage;
  * @author Magnus Hedlund
  * */
 public class Session implements Runnable{
-	private final static int PLAYER0 =0, PLAYER1=1, SUBMARINES=5, DESTROYERS=3, AIRCRAFT_CARRIERS=1;
+	
+	/**
+	 * Constant for first players index, used for accessing arrays
+	 */
+	private final static int PLAYER0 =0;
+	
+	/**
+	 * Constant for second players index, used for accessing arrays
+	 */
+	private final static int PLAYER1=1; 
+	
+	/**
+	 * Constant for number of Submarines
+	 */
+	private final static int SUBMARINES=5;
+	
+	/**
+	 * Constant for number of Destroyers
+	 */
+	private final static int DESTROYERS=3; 
+	
+	/**
+	 * Constant for number of Aircraft_Carriers
+	 */
+	private final static int AIRCRAFT_CARRIERS=1;
+	
+	/**
+	 * array of Player objects. Contains active players or null
+	 * */
 	private Player[] player = new Player[2];
+	
+	/**
+	 * boolean for showing if Navy objects are valid
+	 */
 	private boolean[] navyValid = new boolean[2];
+	
+	/**
+	 * The players Navy objects, the objects to manipulate
+	 */
 	private Navy[] navy = new Navy[2];
+	
+	/*
+	 * variables to keep track of active player 
+	 */
 	private int currentPlayer=PLAYER0, otherPlayer=PLAYER1;
-	private boolean isHit=false;
-	private boolean isSunk=false;
-	private boolean grantTurn=false;
-	private boolean finished=false;
+
+	/**
+	 * The ServerAI, only used when playing against server
+	 */
 	private ServerAI serverAI=null;
 	
+	/**
+	 * Constructor for starting session player VS player
+	 * 
+	 * @param Player ,first player
+	 * @param Player ,second player
+	 * */
 	public Session(Player first, Player second){
 		player[PLAYER0]=first;
 		player[PLAYER1]=second;
 	}
 	
+	/**
+	 * Constructor for starting session against ServerAI
+	 * @param Player ,the player
+	 * */
 	public Session(Player first){
 		player[PLAYER0]=first;
 		serverAI = new ServerAI(5,3,1);
@@ -45,7 +95,6 @@ public class Session implements Runnable{
 		System.out.println("Run");
 		/* listen and validate Navy objects*/
 		while(!navyValid[PLAYER0] || !navyValid[PLAYER1]){
-			System.out.println("In Whileloop");
 			if(!navyValid[PLAYER0]){
 				boolean isValid = readAndValidate(PLAYER0);
 				navyValid[PLAYER0]=isValid;
@@ -60,7 +109,6 @@ public class Session implements Runnable{
 				System.out.println("server already valid");
 			}
 			else if(!navyValid[PLAYER1]){
-				System.out.println("validating navy 2");
 				boolean isValid = readAndValidate(PLAYER1);	
 				navyValid[PLAYER1]=isValid;
 				if(isValid){System.out.println("validated navy 2");}
@@ -79,43 +127,41 @@ public class Session implements Runnable{
 	
 	/**
 	 * Listening for NavyMessage and verifies the Navy object.
-	 * 
-	 * */
+	 * @param playerNumber
+	 * @return is Navy valid?
+	 */
 	private boolean readAndValidate(int playerNumber){
 		Validator validator = new Validator(SUBMARINES,DESTROYERS,AIRCRAFT_CARRIERS);
 		Message msg = readMessage(player[playerNumber]);
 		if(msg.getType().equals("NavyMessage")){
-			System.out.println("We have a navy");
 			NavyMessage navMsg = (NavyMessage)msg;
-			if(navMsg.getNavy().allSet()){
-				System.out.println("allSet");
-			}
-			
 			if(validator.validateNavy(navMsg.getNavy())){
-				System.out.println("it´s valid");
 				navy[playerNumber]=navMsg.getNavy();
 				return true;
 			}
 			else{
-				System.out.println("it´s not valid");
 				return false;
 			}
 			
 		}
 		else{
-			System.out.println("We don´t have a navy");
 			return false;
 		}	
 	}
 	
 	
 	/**
-	 * The game loop. Read messages/getshots and evaluate. Messages are sent to clients(not to the ServerAI)
+	 * The game loop. Read messages/get shots and evaluate. Messages are sent to clients(not to the ServerAI)
 	 * */
 	private void enterGameLoop(){
 		boolean loop=true;
 		Coordinate shotCoordinate=null;
 		Ship hitShip=null;
+		boolean isHit=false;
+		boolean isSunk=false;
+		boolean grantTurn=false;
+		boolean finished=false;
+		
 		while(loop){
 			
 			//reset
@@ -134,7 +180,6 @@ public class Session implements Runnable{
 				Message msg = readMessage(player[currentPlayer]);
 				Shot shotMsg=null;
 				if(msg.getType().equals("Shot")){
-					System.out.println("received Shot");
 					shotMsg = (Shot)msg;
 					shotCoordinate = shotMsg.getCoordinate();
 					
@@ -152,7 +197,6 @@ public class Session implements Runnable{
 			
 			// do we have a Coordinate?
 			if(shotCoordinate!=null){
-				System.out.println("X: "+Integer.toString(shotCoordinate.getX())+" Y: "+Integer.toString(shotCoordinate.getY()));
 				
 				// shoot the other players navy
 				hitShip=navy[otherPlayer].shot(shotCoordinate);
@@ -160,7 +204,6 @@ public class Session implements Runnable{
 				//a hit
 				if(hitShip!=null){
 					isHit=true;
-					System.out.println(hitShip.getName());
 					if(!hitShip.isSunk()){
 						hitShip=null;  //don´t send Ship unless sunk
 					}
@@ -175,7 +218,6 @@ public class Session implements Runnable{
 				}
 				// no hit
 				else{
-					System.out.println("Let the other one fire");
 					//if miss  let the other one fire
 					grantTurn=true;
 				}
@@ -238,6 +280,12 @@ public class Session implements Runnable{
 		}
 	}
 	
+	
+	/**
+	 * Reads message. if no message received sleep and check again
+	 * @param p, the Player to read from
+	 * @return msg, the message
+	 */
 	private Message readMessage(Player p){
 		Message msg=null;
 		while(msg==null){
@@ -246,11 +294,8 @@ public class Session implements Runnable{
 				if(msg==null){
 					Thread.sleep(500);
 				}
-				else{
-					System.out.println("Got a message: "+msg.getType());
-				}
 			}catch(Exception e){
-				 
+				 e.printStackTrace();
 			}
 		}
 		return msg;	
